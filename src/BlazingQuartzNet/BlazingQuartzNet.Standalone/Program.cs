5a436132aa75c00e84bb5310f1f5b662efe81527
William Chan <wellkord@yahoo.com>
2022-07-03 15:26:04 +0800

    Standalone app base code

---
+++ src/BlazingQuartzNet/BlazingQuartzNet.Standalone/Program.cs
@@ -0,0 +1,41 @@
+﻿using Quartz;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        // base configuration from appsettings.json
+        services.Configure<QuartzOptions>(hostContext.Configuration.GetSection("Quartz"));
+
+        // if you are using persistent job store, you might want to alter some options
+        services.Configure<QuartzOptions>(options =>
+        {
+            var jobStoreType = options["quartz.jobStore.type"];
+            if ((jobStoreType ?? string.Empty) == "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz")
+            {
+                options.Scheduling.IgnoreDuplicates = true; // default: false
+                options.Scheduling.OverWriteExistingData = true; // default: true
+            }
+
+            var dataSource = options["quartz.jobStore.dataSource"];
+            if (!string.IsNullOrEmpty(dataSource))
+            {
+                var connectionStringName = options[$"quartz.dataSource.{dataSource}.connectionStringName"];
+                var connStr = hostContext.Configuration.GetConnectionString(connectionStringName);
+                options[$"quartz.dataSource.{dataSource}.connectionString"] = connStr;
+            }
+        });
+        // Add the required Quartz.NET services
+        services.AddQuartz(q =>
+        {
+            // Use a Scoped container to create jobs. I'll touch on this later
+            q.UseMicrosoftDependencyInjectionJobFactory();
+
+        });
+        // Add the Quartz.NET hosted service
+        services.AddQuartzHostedService(
+            q => q.WaitForJobsToComplete = true);
+    })
+    .Build();
+
+await host.RunAsync();
+
